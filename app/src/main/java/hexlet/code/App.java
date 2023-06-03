3029cc40fc85081a7d8f7ab7464aa9aaafa5e111
John Wain <tolikeamoveit14@gmail.com>
2023-06-03 16:24:51 +0300

    add checkStyleSet

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
 
-        gamerName = Cli.Greetings();
+        gamerName = Cli.greetings();
 
 
     }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 import java.util.Scanner;
 public class Cli {
-    public static String Greetings() {  //Сбор имени через Scanner
+    public static String greetings() {  //Сбор имени через Scanner
         Scanner gamerName = new Scanner(System.in);
         System.out.println("May I have your name?");
         String retGamerName =  gamerName.nextLine();
